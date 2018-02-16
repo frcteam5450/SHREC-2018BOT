310f06a3dd55bcbb49e72cf07e7a898e968c8fdf
Evan Garrison <evangarr7@gmail.com>
2018-02-16 11:34:01 -0800

    Updated Classes

--- src/org/usfirst/frc/team5450/robot/Autonomous/LeftA.java
+++ src/org/usfirst/frc/team5450/robot/Autonomous/LeftA.java
@@ -1,24 +1,15 @@
 package org.usfirst.frc.team5450.robot.Autonomous;
 import org.usfirst.frc.team5450.robot.RobotCommandFunctions.DriveTrain;
-<<<<<<< HEAD
-=======
-import org.usfirst.frc.team5450.robot.Objects;
->>>>>>> bd84e7d5faee94b050f4da1a7795772740b30476
 
 public class LeftA {
 	
 	static DriveTrain drive = new DriveTrain();
 	
 	public static void leftARight() {
-<<<<<<< HEAD
 		DriveTrain.gyro.reset();
 		drive.driveStraight(.75 , 0.1 , .5);
 		drive.stopDrive();
 		DriveTrain.gyro.reset();
 		drive.turn(.35, -90);
-=======
-		drive.driveStraight(.5 , .01 , 4);
-		drive.stopDrive();
->>>>>>> bd84e7d5faee94b050f4da1a7795772740b30476
 	}
 }
